74b564deda52dd8e932814bb001699886d040112
unknown <apipska.10@gmail.com>
2024-10-20 10:43:38 +0700

    Testing Cron Job

--- src/app/api/schedule-save/route.js
+++ src/app/api/schedule-save/route.js
@@ -33,12 +33,12 @@ export async function GET(request) {
 			}
 		});
 
-		const authHeader = request.headers.get("authorization");
-		if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-			return new Response("Unauthorized", {
-				status: 401,
-			});
-		}
+		// const authHeader = request.headers.get("authorization");
+		// if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+		// 	return new Response("Unauthorized", {
+		// 		status: 401,
+		// 	});
+		// }
 
 		if (dataMap) {
 			for (const item of dataMap) {
